16789122b9e8ff6f5495c5604f6f572a43eb87ea
Momagic11 <69002096+Momagic11@users.noreply.github.com>
2023-02-13 10:54:09 -0500

    Add files via upload

---
+++ basics.php
@@ -0,0 +1,13 @@
+<?php
+// Mixing HTML and PHP
+    $myName = "Mo":
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sample PHP Page</title>
+</head>
+<body>
+Hello, <strong><?=$myName?></strong>!<br>
+</body>
+</html>
